5d7904bdf1acf888de989c519c8a1f0c967ff8a1
Denis <visible@jumph4x.net>
2023-09-14 15:55:56 -0700

    Lightweight OFFIS theme

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def random_bg
+    filenames = Dir.entries("#{Rails.root}/app/assets/images/background/").select{|fn| fn.match /jpg/}
+    filename = filenames[rand(0...filenames.size)]
+    asset_url("background/#{filename}")
+  end
+
 end
--- public/javascript/enhancements.js
+++ public/javascript/enhancements.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   //document.querySelector("[data-category].active [data-submenu]").classList.toggle("hidden");
   //document.querySelector("[data-category].active .arrow").classList.toggle("rotate-180");
 
-  document.querySelector("a[data-sidebar-button]").addEventListener("click", function(e) {
-    document.querySelector(".sidebar").classList.toggle("hidden");
-  });
+  // document.querySelector("a[data-sidebar-button]").addEventListener("click", function(e) {
+  //   document.querySelector(".sidebar").classList.toggle("hidden");
+  // });
 });
