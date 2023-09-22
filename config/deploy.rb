fe933695b0762ec4262604c4785f503d04e1097b
Denis <visible@jumph4x.net>
2023-09-22 13:03:35 -0700

    Persist storage

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ append :linked_files, "config/database.yml"
 append :linked_files, "config/master.key"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "storage"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
